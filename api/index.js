6771cda7ede903fdbe09407a1c38f0f3f0aadb9a
David Rojas Rodriguez <david154x@hotmail.com>
2023-05-13 20:21:09 -0500

    prueba agregar origin de cors

--- api/index.js
+++ api/index.js
@@ -4,13 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(cors({ origin: "https://envio-email.vercel.app" }));
 
 const transporter = nodemailer.createTransport({
   host: "smtp.office365.com",
